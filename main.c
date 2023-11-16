a3670d0d0d21d1ce39ce571a042766301bd73735
Olu433 <calebawe0@gmail.com>
2023-11-16 20:16:13 +0100

    Pushing files

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(void)
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)(uintptr_t)UINTPTR_MAX;
+    addr = (void *)(uintptr_t)UINT_MAX;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
